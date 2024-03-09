da12fd47edd429b1c94822ca93a338b76b5e2406
Rati Jebisashvili <rjebisashvili@gmail.com>
2024-03-09 00:19:11 -0500

    add health and coffee controllers

---
+++ app/controllers/coffee_controller.rb
@@ -0,0 +1,12 @@
+class CoffeeController < ApplicationController
+  def index
+    @result = CoffeeType.includes(:coffee_ingredients).all
+    render json: @result.as_json
+  end
+
+  def show
+    @result = CoffeeType.includes(:coffee_ingredients)
+                        .find_by(id: @params[:id])
+    render json: @result.as_json
+  end
+end
